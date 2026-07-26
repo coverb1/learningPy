0adba4d9765ea352ef971bb8c328d87e4bd922e7
MucyoBruce <cboy85096@gmail.com>
2026-07-26 12:25:49 +0200

    changes

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
-freinds=["kevin","karen","jim"]
-print(freinds[1:3])
+# lucky_number=[4,8,15,16]
+# freinds=["kevin","Karen","jim"]
+# freinds.reverse()
+# print(freinds)
+
+# def sayHi():
+#     print('Hello Bruce')
+
+# sayHi()
+
+is_male=False
+is_Tall=True
+
+if is_male or is_Tall :
+    print("you are the male")
+
+else:
+    print("you are the female")
