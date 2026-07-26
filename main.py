81749f87054383dca7c436dfd5a602a2bd2324c4
MucyoBruce <cboy85096@gmail.com>
2026-07-26 13:18:30 +0200

    changes

--- main.py
+++ main.py
@@ -5,14 +5,32 @@
 
 # def sayHi():
 #     print('Hello Bruce')
-
 # sayHi()
+# is_male=False
+# is_Tall=True
 
-is_male=False
-is_Tall=True
+# if is_male or is_Tall :
+#     print("you are the male")
+# elif is_male and is_Tall:
+#     print("You are cover boy")
+# else:
+#     print("you are the female")
 
-if is_male or is_Tall :
-    print("you are the male")
 
-else:
-    print("you are the female")
+# def max_num(num1,num2,num3):
+#     if num1>=num2 and num1>=num3:
+#         return num1
+#     elif num2>=num1 and num2>=num3:
+#         return num2
+#     else:
+#         return num3
+
+# print(max_num(2,3,4))
+
+# freinds=['Mucyo',"bruce"]
+# for freind in freinds:
+#     print(freind)
+total=0
+for numbers in range(1,6):
+    total=total+numbers
+    print(total)
